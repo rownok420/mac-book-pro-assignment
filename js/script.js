5479b78960e9c7c0d5adecc180cff18b831b73e7
rownok420 <mrbluebird420@gmail.com>
2021-08-21 00:15:15 +0600

    handle extra cost

--- js/script.js
+++ js/script.js
@@ -4,35 +4,42 @@
 // event linstener for Memory
 
 document.getElementById('free-memory').addEventListener('click',function(){
-    console.log('free')
+    const memoryCost = document.getElementById('extra-memory-cost');
+    memoryCost.innerText = '0';
 })
 
 document.getElementById('cost-memory').addEventListener('click',function(){
-    console.log('cost')
+    const memoryCost = document.getElementById('extra-memory-cost');
+    memoryCost.innerText = '180';
 })
 
 
 // event linstener for Storage
 
 document.getElementById('ssd-256gb').addEventListener('click',function(){
-    console.log('256')
+    const storageCost = document.getElementById('extra-storage-cost');
+    storageCost.innerText = '0'
 })
 
 document.getElementById('ssd-512gb').addEventListener('click',function(){
-    console.log('512')
+    const storageCost = document.getElementById('extra-storage-cost');
+    storageCost.innerText = '100'
 })
 
 document.getElementById('ssd-1tb').addEventListener('click',function(){
-    console.log('1tb')
+    const storageCost = document.getElementById('extra-storage-cost');
+    storageCost.innerText = '180'
 })
 
 
 // event linstener for delivery option
 
 document.getElementById('free-delivery').addEventListener('click',function(){
-    console.log('free-delivery')
+    const deliveryCharge = document.getElementById('delivery-charge');
+    deliveryCharge.innerText = '0';
 })
 
 document.getElementById('cost-delivery').addEventListener('click',function(){
-    console.log('cost-delivery')
+    const deliveryCharge = document.getElementById('delivery-charge');
+    deliveryCharge.innerText = '20';
 })

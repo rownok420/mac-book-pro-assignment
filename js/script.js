7bdcecc391b6e6d2599bbc560dbc93d5356d9292
rownok420 <mrbluebird420@gmail.com>
2021-08-21 00:01:28 +0600

    added event listener all button

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+
+
+// event linstener for Memory
+
+document.getElementById('free-memory').addEventListener('click',function(){
+    console.log('free')
+})
+
+document.getElementById('cost-memory').addEventListener('click',function(){
+    console.log('cost')
+})
+
+
+// event linstener for Storage
+
+document.getElementById('ssd-256gb').addEventListener('click',function(){
+    console.log('256')
+})
+
+document.getElementById('ssd-512gb').addEventListener('click',function(){
+    console.log('512')
+})
+
+document.getElementById('ssd-1tb').addEventListener('click',function(){
+    console.log('1tb')
+})
+
+
+// event linstener for delivery option
+
+document.getElementById('free-delivery').addEventListener('click',function(){
+    console.log('free-delivery')
+})
+
+document.getElementById('cost-delivery').addEventListener('click',function(){
+    console.log('cost-delivery')
+})
